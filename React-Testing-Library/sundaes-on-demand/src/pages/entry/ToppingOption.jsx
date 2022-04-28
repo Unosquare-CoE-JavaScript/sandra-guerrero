78228606734bf90ce4508be3ace5197e19f23f72
Sandra Guerrero <sandra.guerrero@unosquare.com>
2022-04-28 13:04:30 -0500

    Code Quiz Grand Total

--- React-Testing-Library/sundaes-on-demand/src/pages/entry/ToppingOption.jsx
+++ React-Testing-Library/sundaes-on-demand/src/pages/entry/ToppingOption.jsx
@@ -1,13 +1,22 @@
-import { Col } from 'react-bootstrap';
+import { Col, Form } from "react-bootstrap";
 
-export default function ToppingOption({ name, imagePath }) {
+export default function ToppingOption({ name, imagePath, updateItemCount }) {
   return (
     <Col xs={12} sm={6} md={4} lg={3}>
       <img
         src={`http://localhost:3030/${imagePath}`}
         alt={`${name} topping`}
-        style={{ width: '75%' }}
+        style={{ width: "75%" }}
       />
+      <Form.Group controlId={`${name}-topping-checkbox`}>
+        <Form.Check
+          type="checkbox"
+          onChange={(event) => {
+            updateItemCount(name, event.target.checked ? 1 : 0);
+          }}
+        />
+        <Form.Label>{name}</Form.Label>
+      </Form.Group>
     </Col>
   );
 }
