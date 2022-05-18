a78b3ce0aabc64367d510cbdb94c1f6eced17ebc
Sandra Guerrero <sandra.guerrero@unosquare.com>
2022-05-17 22:37:47 -0500

    Nesting <Suspense />

--- concurrent-rendering-adventures-in-react-18/src/components/users/UserDetails.tsx
+++ concurrent-rendering-adventures-in-react-18/src/components/users/UserDetails.tsx
@@ -2,6 +2,8 @@ import { AccountDetails } from './AccountDetails';
 import { MovieDetails } from './MovieDetails';
 import { ErrorBoundary } from "react-error-boundary";
 import { ErrorFallback } from "../ErrorFallback";
+import { Suspense } from 'react';
+import { Loading } from '../Loading';
 
 interface Props {
   userId: number;
@@ -10,14 +12,16 @@ interface Props {
 export function UserDetails({ userId, movieId }: Props) {
   return (
     <div>
-      <h4 className="text-center mt-5">User details</h4>
-      <ErrorBoundary FallbackComponent={ErrorFallback}>
-        <AccountDetails userId={userId} />
-      </ErrorBoundary>
-      <h4 className="text-center mt-5">Favorite movie</h4>
-      <ErrorBoundary FallbackComponent={ErrorFallback}>
-        <MovieDetails movieId={movieId} />
-      </ErrorBoundary>
+      <Suspense fallback={<Loading />}>
+        <h4 className="text-center mt-5">User details</h4>
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+          <AccountDetails userId={userId} />
+        </ErrorBoundary>
+        <h4 className="text-center mt-5">Favorite movie</h4>
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+          <MovieDetails movieId={movieId} />
+        </ErrorBoundary>
+      </Suspense>
     </div>
   );
 }
